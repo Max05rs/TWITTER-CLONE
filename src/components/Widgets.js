62f5df5ab76b9b5dba579a1c4fe867cab83ec726
Max05rs <rajwant0575.be20@chitkara.edu.in>
2022-09-18 14:22:27 +0530

    Widgets

--- src/components/Widgets.js
+++ src/components/Widgets.js
@@ -9,7 +9,31 @@ import SearchIcon from '@material-ui/icons/Search'
 
 function Widgets() {
     return (
-        <h2>hello</h2>
+        <div className = "widgets">
+            <div className = "widgets__input">
+                <SearchIcon 
+                    className = "widgets__SearchIcon" 
+                />
+                <input placeholder = "Search Twitter" type = "text" />
+            </div>
+            <div className = "widgets__widgetContainer">
+                <h2>
+                    Whats'happening
+                </h2>
+                <TwitterTweetEmbed tweetId = {"28023330"} />
+
+                <TwitterTimelineEmbed 
+                    sourceType = "profile"
+                    screenName = "Crunchyroll"
+                    options = {{ height: 400 }}
+                />
+
+                <TwitterShareButton 
+                    url = {"https://facebook.com/akkyProjects"}
+                    options = { { text : "Subscribe my Youtube Channel #CodeWithAkky" , via :  "akkyProjects" }}
+               />
+            </div>
+        </div>
     )
 }
 
